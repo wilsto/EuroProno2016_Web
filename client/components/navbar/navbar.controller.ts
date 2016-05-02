e981927fdf3c6417c4ff1b4d85b92589d09bf239
Ced <Ced9225@gmail.com>
2016-05-02 18:11:04 +0200

    0.2.2

--- client/components/navbar/navbar.controller.ts
+++ client/components/navbar/navbar.controller.ts
@@ -1,26 +1,26 @@
 'use strict';
 
 class NavbarController {
-  //start-non-standard
-  menu = [{
-    'title': 'Home',
-    'state': 'main'
-  }];
+    //start-non-standard
+    menu = [{
+        'title': 'Home',
+        'state': 'main'
+    }];
 
-  isCollapsed = true;
-  //end-non-standard
-  
-  constructor(Auth) {
-    this.isLoggedIn = Auth.isLoggedIn;
-    this.isAdmin = Auth.isAdmin;
-    this.getCurrentUser = Auth.getCurrentUser;
-  }
+    isCollapsed = true;
+    //end-non-standard
 
-  $onInit() {
-    console.log('init navbar');
-  }
-
+    constructor($http, Auth, $translate) {
+        this.$http = $http;
+        this.isLoggedIn = Auth.isLoggedIn;
+        this.isAdmin = Auth.isAdmin;
+        this.getCurrentUser = Auth.getCurrentUser;
+        this.$translate = $translate;
+        //TODO   ajouter recherche de langue pour un user
+        this.lang = navigator.language || navigator.userLanguage;
+        this.$translate.use(this.lang);
+    }
 }
 
 angular.module('euroProno2016WebApp')
-  .controller('NavbarController', NavbarController);
+    .controller('NavbarController', NavbarController);
