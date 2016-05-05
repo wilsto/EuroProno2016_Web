7d07bcb1bbf9725e1b2cc0175d3b623791182f1f
Ced <Ced9225@gmail.com>
2016-05-05 12:18:41 +0200

    0.2.7c
    initalisation news

--- client/app/news/news.controller.ts
+++ client/app/news/news.controller.ts
@@ -1,17 +1,66 @@
+/// <reference path="../../typings/tsd.d.ts" />
 'use strict';
 (function() {
 
     class NewsComponent {
-        constructor() {
-            this.message = 'Hello';
+
+        menu = [
+            { name: 'Home', href: '/', section: '', ngclick: '', class: 'active', a_class: 'nothing' },
+            { name: 'Admin Users', href: '/admin', section: '', ngclick: '', class: 'active', a_class: 'nothing' }
+        ];
+
+        constructor($http, $ngBootbox) {
+            this.$ngBootbox = $ngBootbox;
+            this.$http = $http;
+            this.news = [];
         }
+
+        $onInit() {
+                console.log('init');
+
+                // on récupère les pronos du joueur sinon on crèe le squelette
+                this.$http.get('/api/newss').then(response => {
+                    try {
+                        this.news = _.sortBy(response.data, ['page', 'en']);
+                    } catch (err) {
+                        console.log('vide');
+                    }
+                });
+            }
+            //sauvegarde les pronos
+        createNews(form) {
+            // si trad existe déjà
+            console.log(form.$valid);
+            if (form.$valid) {
+
+                this.$http.post('/api/newss', {
+                    group: this.newinfo.group,
+                    title: this.newinfo.title,
+                    info: this.newinfo.info,
+                    image: this.newinfo.image
+                }).then(response => {
+                    this.newinfo.group = '';
+                    this.newinfo.title = '';
+                    this.newinfo.info = '';
+                    this.newinfo.image = '';
+                });
+            }
+        }
+
+        delete(news, index) {
+            this.news.splice(index, 1);
+            this.$http.delete('/api/newss/' + news._id).then(response => {
+                console.log('delete', response);
+            });
+        }
+
     }
 
     angular.module('euroProno2016WebApp')
         .component('news', {
             templateUrl: 'app/news/news.html',
             controller: NewsComponent,
-            controllerAS: 'vm'
+            controllerAs: 'vm'
         });
 
 })();
