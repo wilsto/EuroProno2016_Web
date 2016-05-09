00864a5997fca2f92991bd94c5d660c438f65c1e
Ced <Ced9225@gmail.com>
2016-05-09 17:53:20 +0200

    0.2.8c
    nouvelle version news

--- client/app/news/news.controller.ts
+++ client/app/news/news.controller.ts
@@ -18,27 +18,45 @@
         $onInit() {
                 console.log('init');
 
+
                 // on récupère les pronos du joueur sinon on crèe le squelette
                 this.$http.get('/api/newss').then(response => {
                     try {
-                        this.news = _.sortBy(response.data, ['page', 'en']);
+
+                        this.news = response.data;
+
+                        // création des groupes à partir des infos news
+                        this.types = _.sortBy(_.uniq(_.map(this.news, element => {
+                            return { type: element.type, order: element.type, group: element.group };
+                        }), 'type'), 'order');
+                        console.log("typeone", this.types);
+                        // création des type1 à partir des infos news
+                        // this.typones = _.sortBy(_.uniq(_.map(this.news, element => {
+                        //     return element.type = "1";
+                        // }), 'group'), 'group');
+                        // 
+
+
+
                     } catch (err) {
                         console.log('vide');
                     }
                 });
             }
-            //sauvegarde les pronos
+            // create news 
         createNews(form) {
             // si trad existe déjà
             console.log(form.$valid);
             if (form.$valid) {
 
                 this.$http.post('/api/newss', {
+                    type: this.newinfo.type,
                     group: this.newinfo.group,
                     title: this.newinfo.title,
                     info: this.newinfo.info,
                     image: this.newinfo.image
                 }).then(response => {
+                    this.newinfo.type = '';
                     this.newinfo.group = '';
                     this.newinfo.title = '';
                     this.newinfo.info = '';
