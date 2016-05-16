f5105de2289e8b4cfab08325b5519b74b1259e0a
wilsto <willy.stophe.pro@gmail.com>
2016-05-16 23:34:40 +0200

    Merge remote-tracking branch 'refs/remotes/origin/develop'
    Conflicts:
    	client/app/news/news.controller.ts
    	client/app/news/news.html

--- server/api/league/league.model.js
+++ server/api/league/league.model.js
@@ -3,9 +3,27 @@
 import mongoose from 'mongoose';
 
 var LeagueSchema = new mongoose.Schema({
-  name: String,
-  info: String,
-  active: Boolean
+    name: String,
+    /* 1 private 0 public*/
+    status: Number,
+    /*1 gratuit/ 2 avec mise*/
+    type: Number,
+    description: String,
+
+    owner_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    /*members: mongoose.Schema.Types.Mixed,*/
+
+    members: mongoose.Schema.Types.Mixed,
+    /*    [{
+            user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+            validated: {
+                type: Boolean,
+                default: false
+            }
+        }],*/
+    active: Boolean
+}, {
+    strict: false
 });
 
 export default mongoose.model('League', LeagueSchema);
