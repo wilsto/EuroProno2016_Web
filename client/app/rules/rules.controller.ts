3e6bbe32de56c3490d17daffd67f24007eb8f697
wilsto <willy.stophe.pro@gmail.com>
2016-06-05 10:00:41 +0200

    Menu + image

--- client/app/rules/rules.controller.ts
+++ client/app/rules/rules.controller.ts
@@ -6,9 +6,11 @@
             { name: 'Prono', href: '/prono', section: '', ngclick: '', class: 'nothing', a_class: 'nothing' },
             { name: 'Arena', href: '/arena', section: '', ngclick: '', class: 'nothing', a_class: 'nothing' },
             { name: 'Leagues', href: '/league', section: '', ngclick: '', class: 'nothing', a_class: 'nothing' },
+            { name: 'Players', href: '/player', section: '', ngclick: '', class: 'nothing', a_class: 'nothing' },
             { name: 'Rules', href: '/rules', section: '', ngclick: '', class: 'active', a_class: 'nothing' },
             { name: 'Stats', href: '/stats', section: '', ngclick: '', class: 'nothing', a_class: 'nothing' }
         ];
+
 
         constructor() {
             this.message = 'Hello';
