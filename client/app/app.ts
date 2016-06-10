6cfbedfd654c1d81460e407e0aafa66507c64711
wilsto <willy.stophe.pro@gmail.com>
2016-06-10 11:30:31 +0200

    IE11 : problème de cache

--- client/app/app.ts
+++ client/app/app.ts
@@ -29,6 +29,22 @@ angular.module('euroProno2016WebApp', [
         $translateProvider.preferredLanguage(navigator.language.substring(0, 2) || navigator.userLanguage.substring(0, 2) || 'en');
 
     })
+
+.config(['$httpProvider', function($httpProvider) {
+        //initialize get if not there
+        if (!$httpProvider.defaults.headers.get) {
+            $httpProvider.defaults.headers.get = {};
+        }
+
+        // Answer edited to include suggestions from comments
+        // because previous version of code introduced browser-related errors
+
+        //disable IE ajax request caching
+        $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+        // extra
+        $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+        $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+    }])
     .run(function($rootScope, $touch) {
         $rootScope.language = navigator.language.substring(0, 2) || navigator.userLanguage.substring(0, 2) || 'en';
         $touch.ngClickOverrideEnabled(true);
